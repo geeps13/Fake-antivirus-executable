d57e9837919a762d8a3faad4cf35f4d1260f7c12
geeps13 <65930111+geeps13@users.noreply.github.com>
2020-07-05 16:10:11 +1000

    Here is the bat file for the fake antivirus.

---
+++ VIRUS SCAN.bat
@@ -0,0 +1,107 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAhAXguNAE+1EbsQ5+n//NaOr0waUfEAUKj45b2NL+5d40brFQ==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBNQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZkkaHUrWXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIWCTBxDCWsCEKML4cp24g=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCWsCEKML4cp24g=
+::fBEirQZwNQPfEVWB+kM9LVsJDCKjF076J5sowd7S3d+0wg==
+::cRolqwZ3JBvQF1fEqQIWCTBxDCWsCEKML4cp24g=
+::dhA7uBVwLU+EWBSq4U48SA==
+::YQ03rBFzNR3SWATEuWwlJBc0
+::dhAmsQZ3MwfNWATE100gMQldSwyWfCb6IZA52NuqvKrV8h9NNA==
+::ZQ0/vhVqMQ3MEVWAtB9wZTVBQAji
+::Zg8zqx1/OA3MEVWAtB9wZTVBQAji
+::dhA7pRFwIByZRRmw/EsjaBJHDAXCGkqRI/Ud5uHv+eOVt15dXfZ/b47ImrmPJOVT7Er2NZA42DQ6
+::Zh4grVQjdCyDJGyX8VAjFAhAXguNAE+/Fb4I5/jHzsSulg0rfdAKS8HewrHu
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+
+
+echo STARTING SYSTEM SCAN
+pause
+
+
+
+cd C:\Windows\System32\
+
+tree
+
+
+
+echo SCAN 15 percent complete ...
+
+TIMEOUT 10
+
+
+
+
+cd C:\Windows\
+
+tree
+
+
+
+
+echo SCAN 57 percent complete...
+
+TIMEOUT 10
+
+
+cd C:\
+
+tree
+
+
+
+
+echo	SCAN COMPLETE
+
+PAUSE
+
+
+
+
+echo	.....................................................................
+
+
+
+echo	!!!! WARNING !!!!
+
+echo	1276 MALICIOUS FILES DETECTED
+
+echo	3568 SUSPICIOUS FILES DETECTED
+
+echo	!!!! WARNING !!!!
+
+
+
+echo    .....................................................................
+
+
+PAUSE
+
+
+
+
+echo IMPORTANT PLEASE READ THE FOLLOWING MESSAGE:
+
+echo Just kidding this is FAKE! : )
+
+echo It Was made for fun
